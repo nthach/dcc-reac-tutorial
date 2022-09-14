727167f96bb57fb33b2a1661cb02c83a3fb097e2
nthach <nich.thach@hotmail.com>
2022-09-14 10:25:27 -0700

    Created Child Component for DisplayEntires

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import React, {useState} from "react";
+import DisplayEnteries from "./Components/DisplayEntries/DisplayEntries";
+import DisplayEntries from './Components/DisplayEntries/DisplayEntries'
 
 function App() {
+  const[entries, setEntries] = useState([{weight: 175, date: '11/23/2022'}])
+
   return (
     <div>
-      <h3>Hello World!</h3>
+      <DisplayEnteries parentEntries={entries} />
+ 
 
     </div>
   );
